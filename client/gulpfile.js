1f7c792fa0d400c809d5dce002ce7111645b14d9
gmati <gmati.hamdi2014@gmail.com>
2022-11-12 22:12:08 +0100

    footer comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require("gulp")
 const gulpsass=require('gulp-sass')
 const sass=gulpsass(require('sass'))
 
-gulp.task('sass',async function(){
-    gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+//gulp.task('sass',async function(){
+ //   gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+//})
+
+gulp.task('watch',async function(){
+    gulp.watch('src/components/**/*.scss',async function(){
+        gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+  
+    })
 })
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import"../../css/footer/footer.css"
+function Footer(){
+    return(
+        <footer>Gmati-app-2022</footer>
+    )
+}
+
+export default Footer
