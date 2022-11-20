f5b23d11f41eeb1008d98a804ab2a0bc74b90f6a
gmati <gmati.hamdi2014@gmail.com>
2022-11-20 22:28:15 +0100

    checkout form

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import '../../css/Cart/cart.css'
+import Checkout from '../CheckoutForm/Checkout';
 function Cart(props) {
+    const[showform,setShowform]=useState(false);
+    const[value,setValue]=useState("");
+    const submitOrder =(e)=>{
+        e.preventDefault();
+        const order={
+            name:value.name,
+            email:value.email
+        }
+        console.log(order)
+    }
+    const handleChange =(e)=>{
+        setValue((prevState)=>({...prevState,[e.target.name]:e.target.value}))
+    }
   return (
     <div className='cart-wrapper'>
     <div className='cart-title'>{props.cartItem.length === 0 ? 'Empty Card' : <p>
         there is {props.cartItem.length} product in cart
     </p> }</div>
-
+<div className='cart-items'>
 {props.cartItem.map(p=>(
 
     <div className='cart-item'>
@@ -24,9 +38,19 @@ function Cart(props) {
 
 ))}
    
+</div>
+{ props.cartItem.length !== 0 && 
+(
+<div className='cart-footer'>
+    <div className='total'>Total : $ {props.cartItem.reduce((acc,p)=>{
+        return acc + p.price
+    },0 )}</div>
+    <button onClick={()=>setShowform(true)}>Select products</button>
+</div>
+)
+}
 
-
-
+<Checkout  showform={showform} setShowform={setShowform} submitOrder={submitOrder} handleChange={handleChange}/>
     </div>
   )
 }
