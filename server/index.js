6804cdc87d51324bf2f0bd83ac73a76394e13360
gmati <gmati.hamdi2014@gmail.com>
2022-12-05 21:07:06 +0100

    server cart

--- server/index.js
+++ server/index.js
@@ -3,14 +3,21 @@ const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const Product = require('./models/productModel');
 const router = require('./routes/router');
+const orderRouter = require('./routes/orderRoute');
+
 
 const app=express();
 app.use(bodyParser.json());
-app.use('/',router)
+app.use('/',router);
+app.use('/',orderRouter);
+
 const connectionString='mongodb://localhost:27017/shopping';
 mongoose.connect(connectionString
 //    ,{useNewUrlParser:true,userUnifiedTopology:true}
     ).then(res=>console.log("connection done"))
+
+
+    
 
 app.listen(5001,()=>{
     console.log("Running on port 5001")
---
+++ server/models/orderModel.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose')
+const orderSchema = require('../schema/orderSchema')
+
+const Order=mongoose.model('order',orderSchema)
+module.exports=Order
