7e0d3fb6f911b4bfafca3de01ab3f457d9fa87a1
gmati <gmati.hamdi2014@gmail.com>
2022-12-11 18:41:46 +0100

    deploy

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
-const Product = require('./models/productModel');
 const router = require('./routes/router');
 const orderRouter = require('./routes/orderRoute');
-
+require ('dotenv').config();
 
 const app=express();
 app.use(bodyParser.json());
 app.use('/',router);
 app.use('/',orderRouter);
-
+//mongodb+srv://hamdi:123@react.kjlkfg2.mongodb.net/shopping?retryWrites=true&w=majority
 const connectionString='mongodb://localhost:27017/shopping';
-mongoose.connect(connectionString
+mongoose.connect(process.env.MONGO_URI ||  connectionString
 //    ,{useNewUrlParser:true,userUnifiedTopology:true}
     ).then(res=>console.log("connection done"))
 
-
+if(process.env.NODE_ENV==='production'){
+    app.use('/',express.static('public'));
+    app.get("/",(req,res)=>res.sendFile(__dirname + "/public/index.html"))
+}else { app.get('/',(req,res)=>res.send("api running"))}
     
-
-app.listen(5001,()=>{
+const PORT=process.env.PORT
+app.listen(PORT || 5001,()=>{
     console.log("Running on port 5001")
 })
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,17 +1,19 @@
 const express=require('express')
 const Order = require("../models/orderModel");
 const router=express.Router();
-const app=express();
 
-app.get('/api/orders',async(req,res)=>{
+router.get('/api/orders',async(req,res)=>{
     const orders =await Order.find();
     res.send(orders)
 })
-app.post('/api/orders',async(req,res)=>{
-    const order =await new Order(req.body).save();
+router.post('/api/orders',async(req,res)=>{
+   // const order = await new Order(req.body).save();
+   const neworder= new Order(req.body);
+    const order=await neworder.save();
+   // res.send(saveP)
     res.send(order)
 })
-app.delete('/api/orders/:id',async(req,res)=>{
+router.delete('/api/orders/:id',async(req,res)=>{
     const deletedOrder =await Order.findByIdAndDelete(req.params.id);
     res.send(deletedOrder)
 })
